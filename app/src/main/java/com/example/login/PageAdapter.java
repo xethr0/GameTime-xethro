dcba9700c05d90775ba6b12fe8db28a78f1208e2
carlos Meza <carlos.meza82@yahoo.com>
2021-04-23 19:55:31 -0700

    Fragments and classes

---
+++ app/src/main/java/com/example/login/PageAdapter.java
@@ -0,0 +1,45 @@
+package com.example.sample;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+    private int numOfTabs;
+
+
+
+    public PageAdapter(@NonNull FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs= numOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Profile();
+            case 1:
+                return new Calendar();
+            case 2:
+                return new GameFeed();
+            case 3:
+                return new SearchBar();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
+}
